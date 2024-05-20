be59a438b8222f9943f5b15f8be72e4f2f6e5d2f
o.smirnov <o.smirnov@fun-box.ru>
2024-05-20 09:33:56 +0300

    configure passport

--- backend/index.js
+++ backend/index.js
@@ -3,19 +3,55 @@ import http from "http";
 import cors from "cors";
 import dotenv from "dotenv";
 
+import passport from "passport";
+import session from "express-session";
+import connectMongo from "connect-mongodb-session";
+
+import { buildContext } from "graphql-passport";
 import { ApolloServer } from "@apollo/server";
 import { expressMiddleware } from "@apollo/server/express4";
 import { ApolloServerPluginDrainHttpServer } from "@apollo/server/plugin/drainHttpServer";
-import { connectDB } from "./db/connectDB.js";
 
+import { configurePassport } from "./passport/passport.config.js";
+import { connectDB } from "./db/connectDB.js";
 import { mergedResolvers } from "./resolvers/index.js";
 import { mergedTypeDefs } from "./typeDefs/index.js";
 
 dotenv.config();
+configurePassport();
 
 const app = express();
 
 const httpServer = http.createServer(app);
+
+const MongoDBStore = connectMongo(session);
+
+const store = new MongoDBStore({
+    uri: process.env.MONGO_URI,
+    collection: "sessions",
+});
+
+store.on('error', error => console.log(error))
+
+app.use(
+    session({
+        secret: process.env.SESSION_SECRET,
+        resave: false,
+        saveUninitialized: false,
+        cookie: {
+            maxAge: 1000*60*60*24*7,
+            httpOnly: true,
+        },
+        store,
+    })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+store.on("error", error => {
+    console.log(`MongoDB connection error: ${error}`);
+})
 
 const server = new ApolloServer({
   typeDefs: mergedTypeDefs,
@@ -28,9 +64,12 @@ await server.start();
 app.use(
   '/',
   express.json(),
-  cors(),
+  cors({
+      origin: "http:localhost:3000",
+      credentials: true,
+  }),
   expressMiddleware(server, {
-    context: async({ req }) => ({ req }),
+    context: async({ req, res }) => buildContext({ req, res }),
   }),
 );
 
---
+++ backend/passport/passport.config.js
@@ -0,0 +1,47 @@
+import passport from "passport";
+import bcrypt from "bcryptjs";
+
+import { GraphQLLocalStrategy } from "graphql-passport";
+
+import { User } from "../models/user.model.js";
+
+export const configurePassport = async() => {
+    passport.serializeUser((user, done) => {
+        console.log("Serializing user");
+        done(null, user.id);
+    });
+
+    passport.deserializeUser(async (id, done) => {
+        console.log("Deserializing user");
+        try {
+            const user = await User.findById(id);
+
+            done(null, user);
+        } catch (error) {
+            done(error);
+        }
+
+    });
+
+    passport.use(
+        new GraphQLLocalStrategy(async(username, password, done) => {
+            try {
+                const user = await User.findOne({ username });
+
+                if (!user) {
+                    throw new Error("Invalid username or password");
+                }
+
+                const isValidPassword = await bcrypt.compare(password, user.password);
+
+                if (!isValidPassword) {
+                    throw new Error("Invalid username or password");
+                }
+
+                return done(null, user);
+            } catch (error) {
+                return done(error);
+            }
+        })
+    )
+}
